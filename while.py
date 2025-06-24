e72e4b909f291a8c701b9c1f9d05f214118916a6
Afifah Hadi Lestari <144543580+afifahhadie@users.noreply.github.com>
2025-06-24 10:36:13 +0700

    Add files via upload

---
+++ while.py
@@ -0,0 +1,28 @@
+# versi while loop
+
+hitung = 1
+
+print("kita mulai hitung")
+
+while hitung <= 5:
+    print("misal ini ke ", hitung)
+    hitung += 1
+
+print("udah selesai")
+
+# versi for loop
+
+print("kita mulai hitung")
+
+for hitung1 in range(1,6):
+    print("misal ini ke ", hitung1)
+
+print("udah selesai")
+
+list_1 = ["apel", "semangka", "jeruk"]
+
+set1 = {"apel", "semangka", "jeruk"}
+
+tuple1 = ("apel", "semangka", "jeruk")
+
+dictionary1 = {"buah": "apel", "hewan": "ayam"}
